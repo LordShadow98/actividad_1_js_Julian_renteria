d75a43624ec86e2ca28e7446bb282e4f7c4fa5c6
LordShadow98 <juliancodigoc13@gmail.com>
2023-08-01 20:57:33 -0500

    No se que hago jaja

--- Index.js
+++ Index.js
@@ -3,4 +3,4 @@ const lastName = 'Renteria';
 const Age = 25  ;
 const city = 'Medellin';
 
-console.log(`Hola, mi nombre es ${name} ${lastName}, Vivo en la Ciudad ${city}, y tengo ${Age} años de edad.`)
+console.log(`Hola, mi nombre es ${name} ${lastName}, Vivo en la Ciudad ${city}, y tengo ${Age} años de edad.`);
