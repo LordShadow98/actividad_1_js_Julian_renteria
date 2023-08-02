47fe97b3d14feb5f271c19c6e44d9f9dbfbd4622
LordShadow98 <juliancodigoc13@gmail.com>
2023-08-01 20:34:22 -0500

    Cambio birthYear x Age

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
 const name = 'Julian';
 const lastName = 'Renteria';
-const birthYear = 25  ;
+const Age = 25  ;
 const city = 'Medellin';
 
-console.log(`Hola, mi nombre es ${name} ${lastName}, Vivo en la Ciudad ${city}, y tengo ${birthYear} años de edad. :)`);
+console.log(`Hola, mi nombre es ${name} ${lastName}, Vivo en la Ciudad ${city}, y tengo ${Age} años de edad. :)`);
