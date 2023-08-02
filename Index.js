52f1ab49e19cb9ca15fe662305bf8528c774a5cc
LordShadow98 <juliancodigoc13@gmail.com>
2023-08-01 19:31:12 -0500

    actividad_1_js_nombre_apellido

---
+++ Index.js
@@ -0,0 +1,6 @@
+const name = 'Julian';
+const lastName = 'Renteria';
+const birthYear = 25  ;
+const city = 'Medellin';
+
+console.log(`Hola, mi nombre es ${name} ${lastName}, Vivo en la Ciudad ${city}, y tengo ${birthYear} años de edad.`);
